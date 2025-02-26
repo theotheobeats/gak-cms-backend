3b9d0b073cf53af00ff928be92e0cb432f333b1f
Theo <asakvsa.idn@gmail.com>
2025-02-26 18:56:55 +0700

    initialize project with TypeScript, authentication setup, and PostgreSQL schema

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import { Hono } from "hono";
+import { auth } from "./lib/auth";
+import { cors } from "hono/cors";
+const app = new Hono<{
+	Variables: {
+		user: typeof auth.$Infer.Session.user | null;
+		session: typeof auth.$Infer.Session.session | null;
+	};
+}>();
+
+app.use(
+	"*",
+	cors({
+		origin: "http://localhost:3001", // Replace with your frontend URL
+		allowHeaders: ["Content-Type", "Authorization"],
+		allowMethods: ["POST", "GET", "OPTIONS"],
+		exposeHeaders: ["Content-Length"],
+		maxAge: 600,
+		credentials: true,
+	})
+);
+
+// This middleware will run on every request
+app.use("*", async (c, next) => {
+	const session = await auth.api.getSession({ headers: c.req.raw.headers });
+
+	if (!session) {
+		c.set("user", null);
+		c.set("session", null);
+		return next();
+	}
+
+	c.set("user", session.user);
+	c.set("session", session.session);
+	return next();
+});
+
+// better-auth mounting
+app.on(["POST", "GET"], "/api/auth/*", (c) => {
+	return auth.handler(c.req.raw);
+});
+
+// ENTRY POINT
+app.get("/", (c) => {
+	return c.text("Hello Hono!");
+});
+
+export default {
+	port: 3001,
+	fetch: app.fetch,
+};
---
+++ src/lib/auth.ts
@@ -0,0 +1,15 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "@prisma/client";
+import { openAPI } from "better-auth/plugins";
+
+const prisma = new PrismaClient();
+export const auth = betterAuth({
+	database: prismaAdapter(prisma, {
+		provider: "postgresql",
+	}),
+	emailAndPassword: {
+		enabled: true,
+	},
+	plugins: [openAPI()],
+});
